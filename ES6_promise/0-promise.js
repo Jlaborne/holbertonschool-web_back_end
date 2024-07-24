c29417a5929ec48c57afed3d1427af6f869d1b83
Jlaborne <laborne.j@hotmail.fr>
2024-07-24 12:21:26 +0200

    Task 4 and 5

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // You can add your API call logic here or just resolve immediately.
-        resolve();
-    });
+  return new Promise((resolve) => {
+    // You can add your API call logic here or just resolve immediately.
+    resolve();
+  });
 }

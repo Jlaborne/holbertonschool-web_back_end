13d3cf09f225413d7fdd943e6686f2cf008ab785
Jlaborne <laborne.j@hotmail.fr>
2024-07-24 11:40:44 +0200

    10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,15 @@
 export default function updateUniqueItems(map) {
-  for (const [key, value] of map.entries()) {
-    // Check if the quantity is 1 and update it to 100
-    if (value === 1) {
-      map.set(key, 100);
+    // Check if the input is a Map
+    if (!(map instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+  
+    // Iterate over the map entries
+    for (const [key, value] of map.entries()) {
+      // Check if the quantity is 1 and update it to 100
+      if (value === 1) {
+        map.set(key, 100);
+      }
     }
   }
-}
+  

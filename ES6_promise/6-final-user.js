81d016749e4d024a6a1656871e94905250820fa2
Jlaborne <laborne.j@hotmail.fr>
2024-07-24 16:20:28 +0200

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  // Call both functions and get their promises
+  const signUpPromise = signUpUser(firstName, lastName);
+  const photoUploadPromise = uploadPhoto(fileName);
+
+  // Use Promise.allSettled to handle both promises
+  return Promise.allSettled([signUpPromise, photoUploadPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,

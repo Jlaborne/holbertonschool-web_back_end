d12a1448bf9b3975a87143579595a1fad1f6d9c5
Jlaborne <laborne.j@hotmail.fr>
2024-08-16 18:01:59 +0200

    pycodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,11 +9,10 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
-    
+    Asynchronous coroutine that waits for a random delay
+    between 0 and max_delay seconds.
     Args:
-        max_delay (int): The maximum delay in seconds (inclusive). Default is 10.
-
+        max_delay (int): The maximum delay in seconds. Default is 10.
     Returns:
         float: The actual delay time in seconds.
     """
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,14 +8,14 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawn wait_random n times with the specified max_delay and return the list of all the delays in ascending order.
-
+    Spawn wait_random n times with the specified max_delay
+    and return the list of all the delays in ascending order.
     Args:
         n (int): The number of times to call wait_random.
         max_delay (int): The maximum delay for each wait_random call.
-
     Returns:
         List[float]: A list of all the delays in ascending order.
     """

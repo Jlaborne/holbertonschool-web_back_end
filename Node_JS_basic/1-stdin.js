a1b515eebd1bcb6f95914f112bac2fdcd73a67e1
Jlaborne <laborne.j@hotmail.fr>
2024-09-18 14:27:31 +0200

    task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 // Event listener for when the user inputs their name
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 
   // Close the input stream to allow the program to finish

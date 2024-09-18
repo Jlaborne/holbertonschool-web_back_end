fa21f947037c3a5cbb278c7f14542422cedd28f0
Jlaborne <laborne.j@hotmail.fr>
2024-09-18 14:51:51 +0200

    T4-T5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -64,9 +64,7 @@ const app = http.createServer((req, res) => {
 });
 
 // Listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 // Export the app variable
 module.exports = app;

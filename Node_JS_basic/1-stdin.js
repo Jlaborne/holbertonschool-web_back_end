19e4d4193302521a5ae11077294b7ab8b506eaef
Jlaborne <laborne.j@hotmail.fr>
2024-09-20 11:34:52 +0200

    t1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
-// Display the welcome message
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) process.stdout.write(`Your name is: ${data.toString()}`);
+});
 
-// Read from stdin
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim(); // Get the input and remove any extra spaces/newlines
-
-  // Output the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Check if the input is coming from echo (piped input) or interactive terminal
-  if (process.stdin.isTTY) {
-    process.exit(); // Exit for interactive mode
-  } else {
-    console.log('This important software is now closing');
-    process.exit(); // Exit for piped input
-  }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });

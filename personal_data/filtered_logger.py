98fbc5a2edb55e727b85476fea8d7b53ecfd0b0e
Jlaborne <laborne.j@hotmail.fr>
2025-02-12 10:57:32 +0100

    t1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,23 @@ Module for filtering sensitive personal data from log messages.
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, field: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = field
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,

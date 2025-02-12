ef8c88a415312cfe10471a040d194fe986500cf8
Jlaborne <laborne.j@hotmail.fr>
2025-02-12 10:37:59 +0100

    Pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,12 +5,14 @@ Module for filtering sensitive personal data from log messages.
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Replaces field values in a log message with a redacted string.
 
     Args:
-        fields (List[str]): List of field names whose values should be obfuscated.
+        fields (List[str]): List of fields whose values should be obfuscated.
         redaction (str): The string to replace field values with.
         message (str): The log message.
         separator (str): The character separating fields in the message.
@@ -18,5 +20,6 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     Returns:
         str: The obfuscated log message.
     """
-    pattern = f'({"|".join(map(re.escape, fields))})=[^ {separator}]*'
-    return re.sub(pattern, lambda match: f'{match.group(1)}={redaction}', message)
+    pattern = (f'({"|".join(map(re.escape, fields))})=[^ {separator}]*')
+    return re.sub(pattern, lambda match: f'{match.group(1)}={redaction}',
+                  message)

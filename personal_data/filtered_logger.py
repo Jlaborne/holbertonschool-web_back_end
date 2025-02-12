690d91b34b8d8dafb565c2cb80af70a95e02214d
Jlaborne <laborne.j@hotmail.fr>
2025-02-12 11:09:15 +0100

    Pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,18 +8,32 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with specific fields to redact.
+
+        Args:
+            fields (List[str]): List of fields to redact in log messages.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format log messages, redacting sensitive fields.
+
+        Args:
+            record (logging.LogRecord): The log record.
+
+        Returns:
+            str: The formatted log message with redacted fields.
+        """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super().format(record)

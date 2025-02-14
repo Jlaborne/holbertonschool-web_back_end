e2bee0799f8d4dffbbac8e8b5ab122bf70925faa
Jlaborne <laborne.j@hotmail.fr>
2025-02-14 11:30:28 +0100

    t1

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -23,7 +23,7 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ unauthorized handler
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,10 +26,10 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
       - An error 401
     """
-    abort(401, "Unauthorized")
+    abort(401)

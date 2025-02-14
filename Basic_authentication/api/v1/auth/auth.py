0d9b480d4135ea77ba697f239a1794adfa4f926e
Jlaborne <laborne.j@hotmail.fr>
2025-02-14 12:07:40 +0100

    t3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-
 from typing import List, TypeVar
 from flask import request
 
@@ -9,6 +8,8 @@ User = TypeVar('User')
 
 
 class Auth:
+    """ Class to manage the API authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required for a given path.

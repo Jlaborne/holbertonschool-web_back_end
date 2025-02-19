e7e12372151274dbeb8c4125e51d07972fcd953f
Jlaborne <laborne.j@hotmail.fr>
2025-02-19 13:59:40 +0100

    create method to retrieve user ID with session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,7 @@
 ''' Define SessionAuth class. '''
 
 from api.v1.auth.auth import Auth
-import uuid
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -19,6 +19,16 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = str(uuid.uuid4)
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieve an user ID for a given session_id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id

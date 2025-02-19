eed75a2c1678a6baa04aa4a5c5fe52291a318126
Jlaborne <laborne.j@hotmail.fr>
2025-02-19 11:11:14 +0100

    first task

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" DocDocDocDocDocDoc
+"""
+from typing import List, TypeVar
+from flask import request
+
+User = TypeVar('User')
+
+
+class Auth:
+    """ Class to manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for a given path.
+        Currently returns False for all paths; to be implemented later.
+        """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+        if excluded_paths[-1] != '/':
+            excluded_paths += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the Authorization header from the Flask request object.
+        Currently returns None; to be implemented later.
+        """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> User:
+        """
+        Retrieves the current user based on the request.
+        Currently returns None; to be implemented later.
+        """
+        return None
---
+++ Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" DocDocDocDocDocDoc
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth.
+    Currently, this class is empty and serves as a placeholder
+    for future basic authentication implementations.
+    """
+    pass

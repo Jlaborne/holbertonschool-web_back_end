ee54c05b1c6bcf1e2085548fffbcf8a5a96a0f41
Jlaborne <laborne.j@hotmail.fr>
2025-02-21 13:17:15 +0100

    test for t7

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -17,10 +17,10 @@ def login():
     user_password = request.form.get('password')
 
     if not user_email:
-        return jsonify({"error", "email missing"}), 400
+        return jsonify({"error": "email missing"}), 400
 
     if not user_password:
-        return jsonify({"error", "password missing"}), 400
+        return jsonify({"error": "password missing"}), 400
 
     try:
         found_users = User.search({'email': user_email})
@@ -31,13 +31,18 @@ def login():
         return jsonify({"error": "no user found for this email"}), 404
 
     for user in found_users:
-        if user.is_valid_password(user_password):
+        if not user.is_valid_password(user_password):
             return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
 
+    user = found_users[0]
     session_id = auth.create_session(user.id)
 
     response = jsonify(user.to_json())
-    session_name = getenv('SESSION_NAME', '_my_session_id')
+    session_name = getenv('SESSION_NAME')
     response.set_cookie(session_name, session_id)
+
+    print(response)
+
+    return response

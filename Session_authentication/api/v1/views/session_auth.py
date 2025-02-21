e8fc63ef986205a2d643dcc7b1d78e5616013c72
Jlaborne <laborne.j@hotmail.fr>
2025-02-21 11:47:40 +0100

    t7

---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Module of Session authentication views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /auth_session/login
+    Return
+        - Logged in user
+    """
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+
+    if not user_email:
+        return jsonify({"error", "email missing"}), 400
+
+    if not user_password:
+        return jsonify({"error", "password missing"}), 400
+
+    try:
+        found_users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in found_users:
+        if user.is_valid_password(user_password):
+            return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+    session_name = getenv('SESSION_NAME', '_my_session_id')
+    response.set_cookie(session_name, session_id)

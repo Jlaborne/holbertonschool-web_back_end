9f94ccdc060730aee830b3b3288a93ce0d071c53
Jlaborne <laborne.j@hotmail.fr>
2025-02-27 11:03:24 +0100

    T6

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,11 +13,19 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
 
-def register_user(self, email: str, password: str) -> User:
-    """Registers a new user if they do not already exist."""
-    try:
-        self._db.find_user_by(email=email)
-        raise ValueError(f"User {email} already exists")
-    except NoResultFound:
-        hashed_password = self._db._hash_password(password)
-        return self._db.add_user(email, hashed_password)
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user if they do not already exist."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._db._hash_password(password)
+            return self._db.add_user(email, hashed_password)

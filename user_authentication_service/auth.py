866320f528aded7d23cca88eca45602f9c7bd386
Jlaborne <laborne.j@hotmail.fr>
2025-02-27 11:51:33 +0100

    create session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
             return match
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session ID for a user and updates the database."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = str(uuid.uuid4())
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None

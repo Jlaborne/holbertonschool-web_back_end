e52f54652a141d257678074f917ecef98232abbc
Jlaborne <laborne.j@hotmail.fr>
2025-02-27 15:47:24 +0100

    T12

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app for user authentication service.
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -29,6 +29,20 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Login endpoint to create a new session."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    valid = AUTH.valid_login(email, password)
+    if not valid:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":

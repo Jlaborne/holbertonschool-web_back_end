231d64226fd6882b41a5cc8b4c7bea73b17df972
Jlaborne <laborne.j@hotmail.fr>
2025-02-27 10:57:15 +0100

    T5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,9 +4,20 @@
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoresultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt and returns the hashed bytes."""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def register_user(self, email: str, password: str) -> User:
+    """Registers a new user if they do not already exist."""
+    try:
+        self._db.find_user_by(email=email)
+        raise ValueError(f"User {email} already exists")
+    except NoResultFound:
+        hashed_password = self._db._hash_password(password)
+        return self._db.add_user(email, hashed_password)

661ed062c577673db9b47cda5eca7a527645c95c
Jlaborne <laborne.j@hotmail.fr>
2025-02-28 12:00:51 +0100

    update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -92,3 +92,20 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Uses reset token to validate update of users password
+        """
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_pwd = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_pwd,
+                             reset_token=None)

59e4e85b782a3e9fc6280636b37edbf9239c802d
Jlaborne <laborne.j@hotmail.fr>
2025-02-28 13:54:05 +0100

    app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,6 +90,9 @@ def get_reset_password_token() -> str:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user password with a reset token."""
+    form_data = request.form
+
+    print(form_data)
     try:
         email = request.form['email']
         reset_token = request.form['reset_token']

e5c7a4e259a6cba6c75bed5c9f18c840d413f623
Jlaborne <laborne.j@hotmail.fr>
2025-02-28 13:57:55 +0100

    Hotfix last task

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,23 +90,15 @@ def get_reset_password_token() -> str:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user password with a reset token."""
-    form_data = request.form
-
-    print(form_data)
-    try:
-        email = request.form['email']
-        reset_token = request.form['reset_token']
-        new_password = request.form['new_password']
-    except KeyError:
-        abort(400)
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
 
     try:
-        AUTH.update_password(reset_token, new_password)
+        email = AUTH.update_password(
+            reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
-
-    msg = {"email": email, "message": "Password updated"}
-    return jsonify(msg), 200
 
 
 if __name__ == "__main__":
--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -93,19 +93,13 @@ class Auth:
 
         return reset_token
 
-    def update_password(self, reset_token: str, password: str) -> None:
-        """
-        Uses reset token to validate update of users password
-        """
-        if reset_token is None or password is None:
-            return None
-
+    def update_password(self, reset_token: str, new_password: str) -> str:
+        """Update a user's password if the reset token is valid."""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = self._hash_password(new_password)
+            self._db.update_user(
+                user.id, hashed_password=hashed_password, reset_token=None)
+            return user.email
         except NoResultFound:
-            raise ValueError
-
-        hashed_pwd = _hash_password(password)
-        self._db.update_user(user.id,
-                             hashed_password=hashed_pwd,
-                             reset_token=None)
+            raise ValueError("Invalid reset token")

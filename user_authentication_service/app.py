80de273a30f5b453dc53f8a9264eb2d572689b78
Jlaborne <laborne.j@hotmail.fr>
2025-02-28 13:42:11 +0100

    hotfix

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,15 +90,20 @@ def get_reset_password_token() -> str:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user password with a reset token."""
-    email = request.form("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(400)
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
 
 
 if __name__ == "__main__":

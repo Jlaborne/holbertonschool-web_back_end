90971cdba0ba39517fc5fb0093caadee00632285
Jlaborne <laborne.j@hotmail.fr>
2025-03-03 12:29:45 +0100

    T6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,20 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("test_org")
         self.assertEqual(client._public_repos_url,
                          "https://api.github.com/orgs/test_org/repos")
+
+    @patch("client.get_json")
+    @patch("client.GithubOrgClient._public_repos_url", new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """ Test that public_repos returns the expected list """
+        mock_public_repos_url.return_value = "https://api.github.com/orgs/test_org/repos"
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+        ]
+        client = GithubOrgClient("test_org")
+        expected_repos = ["repo1", "repo2"]
+        self.assertEqual(client.public_repos(), expected_repos)
+
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/test_org/repos")

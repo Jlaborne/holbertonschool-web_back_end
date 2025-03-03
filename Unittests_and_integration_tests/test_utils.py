d282bfa36887a3a7bcc808d13189e41769144422
Jlaborne <laborne.j@hotmail.fr>
2025-03-03 10:26:02 +0100

    Documentation

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+""" File for testing utils.py """
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """ Test Access Nested Map Class """
     @parameterized.expand([
         ("simple_key", {"a": 1}, ("a",), 1),
         ("nested_key", {"a": {"b": 2}}, ("a",), {"b": 2}),

c144f1488e28428e2d41b5bf84b3b5fa64e5b6d1
Jlaborne <laborne.j@hotmail.fr>
2025-03-03 10:22:57 +0100

    Unitest

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ("simple_key", {"a": 1}, ("a",), 1),
+        ("nested_key", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("deep_nested_key", {"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, name, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()

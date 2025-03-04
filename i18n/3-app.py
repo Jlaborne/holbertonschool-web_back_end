0084578c7d401cec795e75ab4f6ebf0996575363
Jlaborne <laborne.j@hotmail.fr>
2025-03-04 10:56:06 +0100

    T3 hotfix

---
+++ i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel and template translation
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+def get_locale():
+    """Determine the best match for supported languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Renders the homepage"""
+    return render_template('3-index.html')
+
+
+@app.context_processor
+def inject_locale():
+    return {"get_locale": get_locale}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

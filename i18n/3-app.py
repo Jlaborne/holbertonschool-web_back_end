677964df48870599a297d8a741ac2b26f7295268
Jlaborne <laborne.j@hotmail.fr>
2025-03-04 11:29:40 +0100

    T3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,70 +1,32 @@
 #!/usr/bin/env python3
-"""
-Flask app with Babel and template translation.
-
-This application sets up a simple web page with internationalization support
-using Flask-Babel. It determines the locale based on the request headers
-and renders the page with the appropriate translations.
-"""
-
+"""Simple flask app setup"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-
-class Config:
-    """
-    Configuration class for Flask app.
-
-    Attributes:
-        LANGUAGES (list): List of supported languages.
-        BABEL_DEFAULT_LOCALE (str): Default locale set to English.
-        BABEL_DEFAULT_TIMEZONE (str): Default timezone set to UTC.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
+from flask_babel import Babel
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Class to set defualts for babel"""
+    LANGUAGES = ["en", "fr"]
+    Babel.default_locale = "en"
+    Babel.default_timezone = "UTC"
+
+
 app.config.from_object(Config)
 
-babel = Babel()
 
-
-def get_locale() -> str:
-    """
-    Determine the best match for supported languages.
-
-    Returns:
-        str: The best match locale based on the request.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-babel.init_app(app, locale_selector=get_locale)
+@babel.localeselector
+def get_locale():
+    """Get best langauge for user"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index() -> str:
-    """
-    Render the homepage.
-
-    Returns:
-        str: Rendered HTML template.
-    """
+def index():
+    """Return simple homepage"""
     return render_template('3-index.html')
 
 
-@app.context_processor
-def inject_locale() -> dict:
-    """
-    Make the `get_locale` function available inside Jinja templates.
-
-    Returns:
-        dict: Dictionary containing the `get_locale` function.
-    """
-    return {"get_locale": get_locale}
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
